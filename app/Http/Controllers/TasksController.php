c319f62f23402a5b82b59eeb8f961432a1242f0c
Moneer99 <moneerbinoon9@gmail.com>
2023-12-31 23:46:15 +0300

    added store method

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTaskRequest;
 use App\Http\Resources\TasksResource;
 use App\Models\Task;
 use App\Traits\HttpResponses;
@@ -30,9 +31,18 @@ class TasksController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreTaskRequest $request)
     {
- 
+         $request->validated();
+
+        $task = Task::create([
+            'user_id' => Auth::user()->id,
+            'name' => $request->name,
+            'description' => $request->description,
+            'priority' => $request->priority
+        ]);
+
+        return new TasksResource($task);
     }
 
     /**
