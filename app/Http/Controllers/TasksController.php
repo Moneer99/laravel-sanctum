ab6810a7a87539338dc5c38a25bb94338d3dc376
Moneer99 <moneerbinoon9@gmail.com>
2023-12-31 23:53:25 +0300

    added update method

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -65,7 +65,13 @@ class TasksController extends Controller
      */
     public function update(Request $request, Task $task)
     {
- 
+         if(Auth::user()->id !== $task->user_id) {
+            return $this->error('', 'You are not authorized to make this request', 403);
+        }
+
+        $task->update($request->all());
+
+        return new TasksResource($task);
      }
 
     /**
